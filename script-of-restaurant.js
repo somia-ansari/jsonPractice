0ed2dc1f3341308b1b8d0075e2e15255aa3d7dd2
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-07-13 15:39:31 +0500

    bat man hotel

---
+++ script-of-restaurant.js
@@ -0,0 +1,373 @@
+let foodForever = {
+    nameOfHotel: "Food Forever",
+    location: {
+        area: "xyz Colony",
+        street: 4,
+        famousPlaceNearBy: "abcMosque"
+    },
+    owner: {
+        name: "Bat man",
+        nationality: "Utopia",
+        cnicNumb: "42401-xxxxxxx-x",
+        email: "abc@gmail.com",
+        contact: "0000000",
+        profession: [
+            "Business Man",
+            "Cartoon Hero",
+            "Bat Man"
+        ],
+        hobbies: [
+            "jumping",
+            "Cartoon",
+            "Movies"
+        ]
+    },
+    website: "www.nothing.com",
+    contactCompany: [
+        {
+            name: "falan falan",
+            contactNumb: "00000-xxxxx",
+            position: "costomer Care"
+        },
+        {
+            name: "Balan Balan",
+            contactNumb: "00000-xxxxx",
+            position: "costomer Care"
+        },
+        {
+            name: "Ala bala",
+            contactNumb: "00000-xxxxx",
+            position: "costomer Care"
+        }
+    ],
+    serviceType:"food",
+    wordsfromOwner: "The Motive of Lasania Chain of Restaurants is being to commit ourselves for providing state of the art services and cuisines for most of the people of the society. We are honoured to serve you as our guest and are proud of our services and staff to make your celeberations as Unique as LASANIA",
+    services: [
+        {
+            type: "Indoor Services",
+            description: "As All of our Restaurants are Alacarte Restaurants we serve Alacarte guests and we have large halls for the purpose. We announce with great pleasure that at Karachi branch our new air conditioned marriage on 2nd Floor has a capacity of 500 seats with beautiful settings of stage & lighting. We have a high speed lift facility. We also serve for Buffet Inside of our Restaurants. We Arrange your Parties inside of our Restaurants For Example Wedding parties, Birthdays, Farewells etc"
+        },
+        {
+            type: "Outdoor  Services",
+            description: "As All of our Restaurants are Alacarte Restaurants we serve Alacarte guests and we have large halls for the purpose. We announce with great pleasure that at Karachi branch our new air conditioned marriage on 2nd Floor has a capacity of 500 seats with beautiful settings of stage & lighting. We have a high speed lift facility. We also serve for Buffet Inside of our Restaurants. We Arrange your Parties inside of our Restaurants For Example Wedding parties, Birthdays, Farewells etc"
+        },
+        {
+            type: "Lunch Boxes",
+            description: "As All of our Restaurants are Alacarte Restaurants we serve Alacarte guests and we have large halls for the purpose. We announce with great pleasure that at Karachi branch our new air conditioned marriage on 2nd Floor has a capacity of 500 seats with beautiful settings of stage & lighting. We have a high speed lift facility. We also serve for Buffet Inside of our Restaurants. We Arrange your Parties inside of our Restaurants For Example Wedding parties, Birthdays, Farewells etc"
+        },
+        {
+            type: "Free Home Delivery",
+            description: "As All of our Restaurants are Alacarte Restaurants we serve Alacarte guests and we have large halls for the purpose. We announce with great pleasure that at Karachi branch our new air conditioned marriage on 2nd Floor has a capacity of 500 seats with beautiful settings of stage & lighting. We have a high speed lift facility. We also serve for Buffet Inside of our Restaurants. We Arrange your Parties inside of our Restaurants For Example Wedding parties, Birthdays, Farewells etc"
+        }
+    ],
+    menu: [
+        {
+            nameOfType: "Traditional Foods",
+            itemTypes: [
+                {
+                    nameOfDrink: "Chicken Green Chili Lemon",
+                    price: 1020
+                 },
+                 {
+                    nameOfDrink: "	Chicken Achar",
+                    price: 1020
+                 },
+                 {
+                    nameOfDrink: "	Chicken Hari Mirch B/L",
+                    price: 1020
+                 },
+                 {
+                    nameOfDrink: "	Chicken Ginger",
+                    price: 1020
+                 },
+                 {
+                    nameOfDrink: "Chicken Masala",
+                    price: 1020
+                 },
+                 {
+                    nameOfDrink: "Chicken Taka Tak",
+                    price: 1020
+                 }
+            ]
+        },
+        {
+            nameOfType: "Hot and Cold Bar",
+            itemTypes: [
+                {
+                    nameOfDrink: "Pina Colada",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Blue Colada",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Strawberry Colada",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Mint Laminate",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Laminate",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Fruit Punch",
+                    price: 200
+                 }
+            ]
+        },
+        {
+            nameOfType: "LASANIA SPECIALTIES",
+            itemTypes: [
+                {
+                    nameOfDrink: "Lasania Special Chicken Handi B/L (Full)",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: " Lasania Special Chicken Handi B/L (Half)",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Chicken Green Handi B/L (Full)",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Chicken Green Handi B/L (Half) ",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Chicken Handi B/L (Full)",
+                    price: 200
+                 },
+                 {
+                    nameOfDrink: "Mutton Handi B/L (Full)",
+                    price: 200
+                 }
+            ]
+        },
+        {
+            nameOfType: "Tandoor",
+            itemTypes: [
+                {
+                    nameOfDrink: "Chicken Naan ",
+                    price: 120
+                 },
+                 {
+                    nameOfDrink: "Qeema Naan",
+                    price: 120
+                 },
+                 {
+                    nameOfDrink: "Garlic Naan",
+                    price: 120
+                 },
+                 {
+                    nameOfDrink: "Tandoori Paratha",
+                    price: 120
+                 },
+                 {
+                    nameOfDrink: "Plain Naan",
+                    price: 120
+                 },
+                 {
+                    nameOfDrink: "Roghni Naan",
+                    price: 120
+                 }
+            ]
+        }
+    ],
+    branchOfRestaurants: [
+        {
+            name: "BatMan Lahore",
+            location: "Murree Road, Rahmanabad, Rawalpindi. Tel: 051-4455733, 051-4845969. Mobile: 0321-5272642",
+            city: "Lahore"
+        },
+        {
+            name: "BatMan Karachi",
+            location: "D-1 Rashid Minhas Road, Karachi. Tel: 021-34991156-57 Fax #: 021-34822555, Mobile: 0321-2060938",
+            city: "Karachi"
+        },
+        {
+            name: "BatMan Rawalpindi",
+            location: "Murree Road, Rahmanabad, Rawalpindi. Tel: 051-4455733, 051-4845969. Mobile: 0321-5272642",
+            city: "Rawalpindi"
+        },
+        {
+            name: "BatMan Faisalabad",
+            location: "Peoples Colony, Susan Road. Faisalabad. Tel: 041-8712220, 041-8712620",
+            city: "Faisalabad"
+        },
+        {
+            name: "BatMan Islamabad",
+            location: "66-Blue Area, Islamabad. Tel: 051-2348180, 051-2348281",
+            city: "Islamabad"
+        }
+    ],
+    reservations:{
+            cause: "Reservation Form For Events and Celebrations",
+            contact: "03332005595",
+            email: "",
+            contactNumb: "",
+            companyNameIfAny: "",
+            landLine: "",
+            typeOfEvent: "",
+            datOfEvent: "",
+            numbOfGuests: "",
+        },
+    welcomeNote: "Warmly Welcome to Lasania Group of Restaurant.",
+    motive: "The Motive of Lasania Chain of Restaurants is being to commit ourselves for providing state of the art services and cuisines for most of the people of the society.",
+    partyMenu: [
+        {
+            name: "Party Menu A",
+            price: "Rs. 1499",
+            foodItems: [
+                "Soup or w/d",
+                "Full Lamb Roast",
+                "Mutton Roast (PCS)",
+                "Chicken Steam Roast",
+                "Mutton Palak"
+            ]
+        },
+        {
+            name: "Party Menu A",
+            price: "Rs. 1199",
+            foodItems: [
+                "Soup or w/d",
+                "Full Lamb Roast",
+                "Mutton Roast (PCS)",
+                "Chicken Steam Roast",
+                "Mutton Palak"
+            ]
+        },
+        {
+            name: "Party Menu B",
+            price: "Rs. 1099",
+            foodItems: [
+                "Soup or w/d",
+                "Full Lamb Roast",
+                "Mutton Roast (PCS)",
+                "Chicken Steam Roast",
+                "Mutton Palak"
+            ]
+        },
+        {
+            name: "Party Menu C",
+            price: "Rs. 990",
+            foodItems: [
+                "Soup or w/d",
+                "Full Lamb Roast",
+                "Mutton Roast (PCS)",
+                "Chicken Steam Roast",
+                "Mutton Palak"
+            ]
+        }
+    ],
+    noteForParty:[
+        "Buffet arrangement will be provided on minimum 50 person.",
+        "Just pay Rs.30/- extra and enjoy welcome drinks.",
+        "50% advance on booking.",
+        "Service charges 5% on total amount.",
+        "Cold drinks Rs.40/- extra charge.",
+        "Mineral water Rs.60/- extra charge."
+    ],
+    timings:[
+        {
+            day: "monday",
+            openTime: "8am",
+            closingTime: "8pm"
+        },
+        {
+            day: "tuesday",
+            openTime: "8am",
+            closingTime: "8pm"
+        },
+        {
+            day: "wednesday",
+            openTime: "8am",
+            closingTime: "8pm"
+        },
+        {
+            day: "thursday",
+            openTime: "8am",
+            closingTime: "8pm"
+        },
+        {
+            day: "friday",
+            openTime: "8am",
+            closingTime: "8pm"
+        },
+        {
+            day: "saturday",
+            openTime: "8am",
+            closingTime: "12am"
+        },
+        {
+            day: "sunday",
+            openTime: "8am",
+            closingTime: "12am"
+        }
+    ],
+    serviceCharges: [
+        {
+            event: "table service",
+            servantCharge:0001,
+
+        },
+        {
+            event: "party service",
+            servantCharge:0001,
+            
+        },
+        {
+            event: "home delivery service",
+            servantCharge:0001,
+            
+        },
+        {
+            event: "lunch box service",
+            servantCharge:0001,
+            
+        }
+    ],
+    expenses:{
+            bills: [
+                "electricity",
+                "sewerage",
+                "water supply",
+                "SUI gas",
+                "Telephone",
+                "internet"
+            ],
+            salaryOf: [
+                "servants",
+                "cleaning boys",
+                "sweepers",
+                "cashier",
+                "manager",
+                "coordinator",
+                "accountant",
+                "peon",
+                "gardener",
+                "exrtra staff"
+            ],
+            maintenace: [
+                "furniture",
+                "curtains",
+                "cleanliness",
+                "atmosphere",
+                "in door plants",
+                "out-door plants"
+            ],
+            servicePaymentFor: [
+                "laundary",
+                "grocery",
+                "green grocery",
+                "friuts",
+                "newspapers",
+                "music"
+            ]
+        }
+
+}
