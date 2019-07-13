abaa35e237c67047bcda960f739a5858bb2548f4
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-07-13 14:41:15 +0500

    page

---
+++ script.js
@@ -0,0 +1,387 @@
+let TechKaro = {
+    courseName: "Tech Karo",
+    fees: 500,
+    schedule: {
+        day: ["Tuesday", "Thursday", "Saturday"],
+        timings: "2pm to 6pm",
+    },
+    staff: [{
+            name: "Sadaffe Abid",
+            position: "Founder of CIRCLE",
+            qualification: "xyz",
+            experience: "15 years",
+            about: "An enthusiastic and worderful women who is keen to work for development of Pakistan specially by empowering women in technical field",
+            gender: "Female",
+            email: "sadafabid@xyz.com",
+            phone: "0333-xx-xx-xxx",
+            github: "www.github.com/sadaffe/",
+            skills: []
+        },
+        {
+            name: "",
+            position: "",
+            qualification: "",
+            experience: "",
+            about: "",
+            gender: "",
+            email: "",
+            phone: "",
+            github: "",
+            skills: []
+        },
+        {
+            name: "",
+            position: "",
+            qualification: "",
+            experience: "",
+            about: "",
+            gender: "",
+            email: "",
+            phone: "",
+            github: "",
+            skills: []
+        },
+        {
+            name: "",
+            position: "",
+            qualification: "",
+            experience: "",
+            about: "",
+            gender: "",
+            email: "",
+            phone: "",
+            github: "",
+            skills: []
+        },
+        {
+            name: "",
+            position: "",
+            qualification: "",
+            experience: "",
+            about: "",
+            gender: "",
+            email: "",
+            phone: "",
+            github: "",
+            skills: []
+        }
+    ],
+    students: [{
+            name: ,
+            gender: ,
+            address: ,
+            dob: ,
+            cnic: ,
+            contact: ,
+            maritalStatus: ,
+            gitHub: ,
+            email: ,
+            group: ,
+            techNinjaName: ,
+            attendance: ,
+            progress: ,
+            hasComputer: ,
+            hasInternet: ,
+            hasSmartphone: ,
+            about: ,
+            practiceHours: ,
+            profession: {
+                position: "",
+                workingHours: "",
+                distanceToUitInKm: 0,
+                salary: 0,
+                isComingDirectly: true,
+            }
+        },
+        {
+            name: ,
+            gender: ,
+            address: ,
+            dob: ,
+            cnic: ,
+            contact: ,
+            maritalStatus: ,
+            gitHub: ,
+            email: ,
+            group: ,
+            techNinjaName: ,
+            attendance: ,
+            progress: ,
+            hasComputer: ,
+            hasInternet: ,
+            hasSmartphone: ,
+            about: ,
+            practiceHours: ,
+            profession: {
+                position: "",
+                workingHours: "",
+                distanceToUitInKm: 0,
+                salary: 0,
+                isComingDirectly: true,
+
+            }
+        },
+        {
+            name: ,
+            gender: ,
+            address: ,
+            dob: ,
+            cnic: ,
+            contact: ,
+            maritalStatus: ,
+            gitHub: ,
+            email: ,
+            group: ,
+            techNinjaName: ,
+            attendance: ,
+            progress: ,
+            hasComputer: ,
+            hasInternet: ,
+            hasSmartphone: ,
+            about: ,
+            practiceHours: ,
+            profession: {
+                position: "",
+                workingHours: "",
+                distanceToUitInKm: 0,
+                salary: 0,
+                isComingDirectly: true,
+            }
+        },
+        {
+            name: ,
+            gender: ,
+            address: ,
+            dob: ,
+            cnic: ,
+            contact: ,
+            maritalStatus: ,
+            gitHub: ,
+            email: ,
+            group: ,
+            techNinjaName: ,
+            attendance: ,
+            progress: ,
+            hasComputer: ,
+            hasInternet: ,
+            hasSmartphone: ,
+            about: ,
+            practiceHours: ,
+            profession: {
+                position: "",
+                workingHours: "",
+                distanceToUitInKm: 0,
+                salary: 0,
+                isComingDirectly: true,
+
+            }
+        }
+    ],
+    venue: {
+        place: "Usman Institute of Technology",
+        location: "Main Samama",
+        fomousPlaceNearBy: "Safari Park",
+        localBusRoute: [
+            "G-17",
+            "G-27",
+            "G-3",
+            "Sheraz",
+            "Safari",
+            "G-7"
+        ]
+        labs: [{
+                Room: "Cl-1",
+                Capacitty: "80 students",
+                numbOfPCs: 80
+            },
+            {
+                Room: "Cl-2",
+                Capacitty: "30 students",
+                numbOfPCs: 30
+            },
+            {
+                Room: "Cl-4",
+                Capacitty: "80 students",
+                numbOfPCs: 80
+            },
+            {
+                Room: "Room number 101",
+                Capacitty: "30 students"
+            },
+            {
+                Room: "Room number 104",
+                Capacitty: "30 students"
+            },
+            {
+                Room: "FYP Lab",
+                Capacitty: "25 students",
+                numbOfPCs: 25
+            },
+            {
+                Room: "Auditorium",
+                Capacitty: "150 students"
+            }
+        ],
+
+    },
+    student: [
+        "Somia",
+        "Reena",
+        "Armia",
+        "Maria",
+        "Bazelah",
+        "Safina",
+        "Sarah",
+        "Urooj",
+        "Aster",
+        "Raiba",
+        "Alaina",
+        "Kokab",
+        "Saima",
+        "Areesha",
+        "Batool",
+        "Usama",
+        "Usman",
+        "Ali Raza",
+        "Qasim",
+        "Abdullah",
+        "Nosheen",
+        "Areeba",
+        "Aqsa",
+        "+80 more"
+    ],
+    teacher: "Sarah Ahmed",
+    sponsoredBy: "Engro Vopak Terminal",
+    programBy: "CIRCLE",
+    programDetail: "It has launched a project in Karachi’s Usman Institute of Technology with partnership of Engro Vopak Terminal Limited to provide training to the under-served community especially women.",
+    certifications: [
+        "Free Code Camp",
+        "Tech Karo"
+    ],
+    TechNinjas: [
+        "Khair-un-Nisa",
+        "Noreen",
+        "Ali",
+        "Abeera",
+        "Rabia"
+    ],
+    webAddress: [
+        "www.circle.org",
+        "www.facebook.com/CIRCLE",
+        "twitter.com/CIRCLE2020"
+    ],
+    internshipOpportunities: [
+        "Engro",
+        "SastaTicket.pk",
+        "10Pearls",
+        "iBEX"
+    ],
+    syllabus: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "WordPress"
+    ],
+    events: [
+        "Orientation",
+        "Mentor-Meetups",
+        "Women-Day",
+        "Life-Skill Sessions",
+        "Professional Skills Development Sessions"
+    ],
+    rewards: [
+        "gift on attendance",
+        "Gifts on good Marks",
+        "Appreciation on good work by faculty",
+        "Appreciation on Challenge completion",
+        "Appreciation on weekly tasks"
+    ],
+    otherResources: [
+        "Free Code Camp Online 3 Courses",
+        "Smarter Way to learn JavaScript Book excercises",
+        "Youtube tutorials"
+    ],
+    classStrategy: [{
+            method: "practice hour",
+            duration: "2 hours daily",
+            days: [
+                "Tuesday",
+                "Thursday",
+                "Saturday"
+            ],
+        },
+        {
+            method: "Teaching hour",
+            duration: "2 hours daily",
+            days: [
+                "Tuesday",
+                "Thursday",
+                "Saturday"
+            ],
+        },
+        {
+            method: "Tests",
+            duration: "Monthly",
+            days: [
+                "Tuesday",
+                "Thursday",
+                "Saturday"
+            ],
+        },
+        {
+            method: "Life Skills",
+            duration: "4 hours weekly",
+            days:"Saturday",
+        }
+    ],
+    homeWork: [
+        "weekly tasks",
+        "Project mokeUps",
+        "Free Code Camp",
+    ],
+    slackURL: "tk-2019",
+    youTubeChannel: "Tech Karo",
+    youTube: [
+        "Playlists by topics",
+        "Practice tasks",
+        "Challenge solutions",
+        "Extra class recordings",
+        "class recording for reference"
+    ],
+    gihubClassResourcesURL: "github.com/sarahsga/repositories/tk-class-resources/",
+    successStories: [{
+        studentName: "Raiba",
+        batch: 1,
+        gender: "Female",
+        currentlyWorking: "Tech Karo",
+        feedback: "She was a teacher earning very less but now she's ambassador at Tech Karo and earning more than her previous salary in less time",
+        salary: 0.001,
+        skills: [
+            "Coding",
+            "Management",
+            "Teaching",
+            "Mentorship",
+            "Communication",
+            "Technical assistance"
+        ]
+    }]
+}
+
+// function showCourse() {
+//     document.querySelector('p').innerHTML = (TechKaro["courseName"]);
+// }
+
+// function org() {
+//     document.querySelector('#org').innerHTML = (TechKaro["programBy"]);
+
+// }
+
+// function opps() {
+//     document.querySelector('#opps').innerHTML = (TechKaro["internshipOpportunities"]);
+
+// }
+
+// function teacher() {
+//     document.querySelector('#opps').innerHTML = (TechKaro["internshipOpportunities"]);
+// }
